5e81ddc34473c399c1c01dfe4f25b21b65765017
Tatsunori Uchino <tats.u@live.jp>
2024-01-21 01:43:47 +0900

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,27 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import { KeyboardTest } from "../components/KeyboardTest";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <>
+      <header>
+        <h1>Keyboard Events Test Page</h1>
+      </header>
+      <main>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
+          This page allows you to test keyboard events including IME-related
+          ones.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+        <KeyboardTest />
+      </main>
+      <footer>
+        <p className={styles.credit}>
+          Developed by{" "}
+          <a href="https://github.com/tats-u" target="_blank" rel="noreferrer">
+            Tatsunori Uchino (@tats-u)
           </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </p>
+      </footer>
+    </>
   );
 }
---
+++ components/KeyboardTest.tsx
@@ -0,0 +1,209 @@
+"use client";
+
+import { useReducer, useState } from "react";
+import styles from "./KeyboardTest.module.scss";
+import clsx from "clsx";
+
+type RegisteredKeyboardEntry = (
+  | {
+      name: "compositionStart" | "compositionUpdate" | "compositionEnd";
+      isComposingEvent?: undefined;
+      isComposingState?: undefined;
+    }
+  | {
+      name: "change";
+      isComposingEvent?: undefined;
+      isComposingState: boolean;
+    }
+  | {
+      name: "keyDown";
+      isComposingEvent: boolean;
+      isComposingState: boolean;
+      count?: number;
+    }
+) & { text: string; id: number };
+
+const maxEventCount = 50;
+
+function getUpdatedQueueWithLimit(
+  q: RegisteredKeyboardEntry[],
+  value: RegisteredKeyboardEntry
+) {
+  if (value.name === "keyDown") {
+    const first = q[0];
+    if (
+      first !== undefined &&
+      first.name === "keyDown" &&
+      value.text === first.text &&
+      value.isComposingEvent === first.isComposingEvent &&
+      value.isComposingState === first.isComposingState
+    ) {
+      return [
+        { ...first, count: (first.count ?? 1) + 1 },
+        ...q.slice(1, maxEventCount),
+      ];
+    }
+  }
+  return [value, ...q.slice(0, maxEventCount - 1)];
+}
+
+function isComposingEventToString(
+  isComposingEvent: boolean | undefined,
+  isComposingState: boolean | undefined
+) {
+  if (!isComposingEvent && !isComposingState) {
+    return "";
+  }
+  return ` (isComposing: ${isComposingEvent ? "[event]" : ""}${
+    isComposingState ? "[state]" : ""
+  })`;
+}
+
+function EventName({ name }: { name: string }) {
+  return <span className={styles.eventName}>{name}</span>;
+}
+
+function CompositionEvent({
+  state: { name, text },
+}: {
+  state: Extract<
+    RegisteredKeyboardEntry,
+    { name: "compositionStart" | "compositionUpdate" | "compositionEnd" }
+  >;
+}) {
+  return (
+    <li className={clsx(styles.listItem, styles[name])}>
+      <EventName name={name} /> <span className={styles.text}>{text}</span>
+    </li>
+  );
+}
+
+function ChangeEventEntry({
+  state: { name, text, isComposingState },
+}: {
+  state: Extract<RegisteredKeyboardEntry, { name: "change" }>;
+}) {
+  return (
+    <li
+      className={clsx(
+        styles.listItem,
+        isComposingState && styles.otherWhenComposing
+      )}
+    >
+      <EventName name={name} /> <span className={styles.text}>{text}</span>
+    </li>
+  );
+}
+
+function KeyDownEventEntry({
+  state: { name, text, isComposingEvent, isComposingState, count },
+}: {
+  state: Extract<RegisteredKeyboardEntry, { name: "keyDown" }>;
+}) {
+  return (
+    <li
+      className={clsx(
+        styles.listItem,
+        isComposingEvent !== isComposingState ? styles.notIntended : styles.otherWhenComposing
+      )}
+    >
+      <EventName name={name} /> <kbd>{text}</kbd>
+      {isComposingEventToString(isComposingEvent, isComposingState)}
+      {(count ?? 1) > 1 && <span className={styles.count}>{count}</span>}
+    </li>
+  );
+}
+
+function KeyboardEvent({ state }: { state: RegisteredKeyboardEntry }) {
+  switch (state.name) {
+    case "compositionStart":
+    case "compositionUpdate":
+    case "compositionEnd":
+      return <CompositionEvent state={state} />;
+    case "change":
+      return <ChangeEventEntry state={state} />;
+    case "keyDown":
+      return <KeyDownEventEntry state={state} />;
+  }
+}
+
+export function KeyboardTest() {
+  const [queue, updateQueue] = useState<RegisteredKeyboardEntry[]>([]);
+  const [isComposing, setIsComposing] = useState(false);
+  const [nextId, updateNextId] = useReducer(
+    (currentId, _?: undefined) => (currentId + 1) & 0xffff,
+    0
+  );
+  return (
+    <div>
+      <form className={styles.form}>
+
+      <input
+        type="text"
+        onChange={(e) => {
+          updateQueue(
+            getUpdatedQueueWithLimit(queue, {
+              name: "change",
+              text: e.target.value,
+              isComposingState: isComposing,
+              id: nextId,
+            })
+          );
+          updateNextId();
+        }}
+        onKeyDown={(e) => {
+          updateQueue(
+            getUpdatedQueueWithLimit(queue, {
+              name: "keyDown",
+              isComposingEvent: e.nativeEvent.isComposing,
+              isComposingState: isComposing,
+              text: e.key,
+              id: nextId,
+            })
+          );
+          updateNextId();
+        }}
+        onCompositionStart={(e) => {
+          setIsComposing(true);
+          updateQueue(
+            getUpdatedQueueWithLimit(queue, {
+              name: "compositionStart",
+              text: e.data,
+              id: nextId,
+            })
+          );
+          updateNextId();
+        }}
+        onCompositionUpdate={(e) => {
+          updateQueue(
+            getUpdatedQueueWithLimit(queue, {
+              name: "compositionUpdate",
+              text: e.data,
+              id: nextId,
+            })
+          );
+          updateNextId();
+        }}
+        onCompositionEnd={(e) => {
+          setIsComposing(false);
+          updateQueue(
+            getUpdatedQueueWithLimit(queue, {
+              name: "compositionEnd",
+              text: e.data,
+              id: nextId,
+            })
+          );
+          updateNextId();
+        }}
+      />
+      <button type="reset">Clear Text</button>
+      <button onClick={() => updateQueue([])}>Clear Log</button>
+      </form>
+      <ul className={styles.list}>
+        {queue.map((e) => (
+          <KeyboardEvent key={e.id} state={e} />
+        ))}
+      </ul>
+    </div>
+  );
+}
