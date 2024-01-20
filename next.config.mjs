ecbc26e86d5b48377793450b686c38311a603ad9
Tatsunori Uchino <tats.u@live.jp>
2024-01-21 02:34:15 +0900

    Fix root path for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+const prefixPath = ((repoName) => {
+  // Not GitHub Actions
+  if (!repoName) return undefined;
+  return repoName.replace(/^[^\/]+/, "");
+})(process.env.GITHUB_REPOSITORY);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: prefixPath,
+  basePath: prefixPath,
 };
 
 export default nextConfig;
