9cf0d00afc029fea5b471d16ad3a480333ba8c31
Tatsunori Uchino <tats.u@live.jp>
2024-01-21 02:41:56 +0900

    Remove assetsPath

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 const prefixPath = ((repoName) => {
   // Not GitHub Actions
   if (!repoName) return undefined;
-  console.log(`Repository name: ${repoName}`);
   return repoName.replace(/^[^\/]+/, "");
 })(process.env.GITHUB_REPOSITORY);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: prefixPath,
   basePath: prefixPath,
 };
 
